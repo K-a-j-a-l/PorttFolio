1535ba0ae171c14b889044b00d4c56a1a07e8bd8
Kajal <Kajal.mittal08@gmail.com>
2023-08-03 08:39:02 +0530

    Add responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
+var sideMenu = document.getElementById("sideMenu");
 
 function openTab(tabname) {
     for (tablink of tabLinks) {
@@ -11,4 +12,14 @@ function openTab(tabname) {
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+function openMenu() {
+    console.log(sideMenu);
+    sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+    sideMenu.style.right = "-200px"
 }
